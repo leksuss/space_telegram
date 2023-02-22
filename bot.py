afbb27ff2a9db44b9dbe865f80544903c36e6558
Andrey Alekseev <leksus@leksus.info>
2023-02-22 23:11:44 +0300

    publish photo to Telegram channel

--- bot.py
+++ bot.py
@@ -7,9 +7,14 @@ env.read_env()
 
 bot = telegram.Bot(token=env('TG_BOT_API_KEY'))
 
-print(bot.get_me())
-
+'''
 bot.send_message(
     chat_id=env('TG_CHAT_ID'),
     text="Привет всем! Тут будут просто космические фотки",
+)
+'''
+
+bot.send_photo(
+    chat_id=env('TG_CHAT_ID'),
+    photo=open('apod_images/saturnrhea_cassini_960.jpg', 'rb'),
 )
